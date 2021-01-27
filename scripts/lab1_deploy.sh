e5ab3b70bf3774c37fe75aa8f912948bb520f77d
Manuel Sanchez <manuss20@gmail.com>
2021-01-27 09:41:27 +0100

    update script

--- scripts/lab1_deploy.sh
+++ scripts/lab1_deploy.sh
@@ -4,16 +4,16 @@ source config.sh
 az login
 az acr login --name $ARC_NAME
 
-echo "Preparando la imagen $CONTAINER_API para depslegar en ACR"
-docker tag $CONTAINER_API $ARC_URL/$CONTAINER_API
+echo "Preparando la imagen $CONTAINER_API:TAG_API para depslegar en ACR"
+docker tag $CONTAINER_API:TAG_API $ARC_URL/$CONTAINER_API:TAG_API
 
-echo "Preparando la imagen $CONTAINER_SQL para depslegar en ACR"
-docker push $ARC_URL/$CONTAINER_API
-echo "La imagen $CONTAINER_API ha sido publicada en ACR"
+echo "Preparando la imagen $CONTAINER_SQL:TAG_SQL para depslegar en ACR"
+docker push $ARC_URL/$CONTAINER_API:TAG_API
+echo "La imagen $CONTAINER_API:TAG_API ha sido publicada en ACR"
 
-echo "Preparando la imagen $CONTAINER_SQL para depslegar en ACR"
-docker tag $CONTAINER_SQL $ARC_URL/$CONTAINER_SQL
+echo "Preparando la imagen $CONTAINER_SQL:TAG_SQL para depslegar en ACR"
+docker tag $CONTAINER_SQL:TAG_SQL $ARC_URL/$CONTAINER_SQL:TAG_SQL
 
-echo "Desplegando la imagen $CONTAINER_SQL"
-docker push $ARC_URL/$CONTAINER_SQL
-echo "La imagen $CONTAINER_SQL ha sido publicada en ACR"
+echo "Desplegando la imagen $CONTAINER_SQL:TAG_SQL"
+docker push $ARC_URL/$CONTAINER_SQL:TAG_SQL
+echo "La imagen $CONTAINER_SQL:TAG_SQL ha sido publicada en ACR"
